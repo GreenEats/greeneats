502f38b9fcb48960f9d4aee06a993742b969e2fa
uween <uweenisirindu@gmail.com>
2020-01-21 14:24:52 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
             <div>
                 <div class="copyright">
                     Copyright &copy; <script>document.write(new Date().getFullYear());</script> GreenEats | All Rights Reserved.
-                </div>
+                </div> 
 			</div>
         </div>
     </footer>
